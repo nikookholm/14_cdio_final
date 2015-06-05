fc14aa31115d9b135ea26ba2c01720d4bf621c56
Onepiece <Onepiece@w79014.web1.wireless.dtu.dk>
2015-06-05 13:17:29 +0200

    Oprettet metoder til ReceptController

--- src/code/client/controllers/ReceptController.java
+++ src/code/client/controllers/ReceptController.java
@@ -2,4 +2,13 @@ package code.client.controllers;
 
 public class ReceptController {
 
-}
+	public void createRecept()
+	{	
+		// hent create recept fra DTO	
+	}
+	
+	public void getReceptList(){		
+	}
+	
+	
+	}
--- src/code/client/controllers/WeightProcedures.java
+++ src/code/client/controllers/WeightProcedures.java
@@ -19,20 +19,29 @@ public class WeightProcedures {
 	private void login()
 	{
 		
+		// aktøren logger ind i systemet. checker for dens ID nr. 
+		
+		
 	}
 	
 	private void confirmOperator()
 	{
+		
+		// som skal godtage operatøren før han kan afveje
 		
 	}
 	
 	private void chooseProductNumber()
 	{
 		
+		// indtastning af produktnummer, som allerede findes i databasen, hvis ikke afviser den
+		
 	}
 	
 	private void ingredientLine(IngredientDTO ingredient)
 	{
+		
+		// udfører de nedstående metoder
 		updateStatus();
 		clearAndTara();
 		enterIngredientBatchNumber();
@@ -41,20 +50,22 @@ public class WeightProcedures {
 	
 	private void clearAndTara()
 	{
+		// clear vægten så man kan tarere
 		
 	}
 	
 	private void updateStatus()
 	{
+		// updatere listen af de indtastede værdier, fx. de afvejde ingredienser
 		
 	}
 	
 	private void enterIngredientBatchNumber()
 	{
+		// indtastning af ingredientsbatch nummeret
+		// henter batch nummerets ingredienser
+		
 		
 	}
-	
-	
-	
 
 }
