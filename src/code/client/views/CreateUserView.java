a6664079ce028b3ea86db96582df267e97b04c50
eggman1979 <kim.d.rasmussen@live.dk>
2015-06-07 19:18:09 +0200

    Har lavet skelettet til CreateUserView

--- src/code/client/views/CreateUserView.java
+++ src/code/client/views/CreateUserView.java
@@ -2,7 +2,9 @@ package code.client.views;
 
 import code.client.DatabaseService;
 import code.client.controllers.MainController;
+import code.database.DALException;
 import code.database.UserDTO;
+import code.shared.FieldVerifier;
 
 import com.google.gwt.dev.asm.tree.IntInsnNode;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -16,75 +18,67 @@ import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class CreateUserView extends Composite {
-
 	VerticalPanel vPanel;
-	Label name, ini, cpr, role;
-	TextBox nameBox, iniBox, cprBox;
-	ListBox roleBox;
-	Grid table;
-	Button okBtn;
-
-	public CreateUserView(final DatabaseService dbs){
-		vPanel = new VerticalPanel();
-		initWidget(vPanel);
-		name = new Label("navn");
-		ini = new Label("ini");
-		cpr = new Label("CPR");
-		role = new Label("Rolle");
-		nameBox = new TextBox();
-		iniBox = new TextBox();
-		cprBox = new TextBox();
-
-		roleBox = new ListBox();
-		roleBox.insertItem("Operatør", 0);
-		roleBox.insertItem("Værkfører", 1);
-		roleBox.insertItem("Farmaceut", 2);
-		roleBox.insertItem("Administrator", 3);
-		vPanel.add(roleBox);
-		
-		table= new Grid(2,4);
-		table.setWidget(0,0, name);
-		table.setWidget(0, 1, nameBox);
-		table.setWidget(1,0, ini);
-		table.setWidget(1, 1, iniBox);
-		table.setWidget(2,0, cpr);
-		table.setWidget(2, 1, cprBox);
-		table.setWidget(3,0, role);
-		table.setWidget(3, 1, roleBox);
-		vPanel.add(table);
-
-
-		okBtn = new Button("Opret");
-		
-
-
-
-	}
 	
+	TextBox nameBox, iniBox, cprBox;
+	ListBox roleList;
+	Button okBtn;
+	Grid table;
 
-	private class okClickHandler implements ClickHandler{
-		UserDTO newPerson = new UserDTO(0, nameBox.getText(), iniBox.getText(), cprBox.getText(), "", roleBox.getTabIndex(), true);
+	MainController mc;
+
+	// Flag der skal afgøre om ok knappen skal gøres tilgænglig
+	boolean nameCheck = false; 
+	boolean iniCheck  = false;
+	boolean cprCheck  = false;
+
+
+
+	public CreateUserView(final MainController mc){
+
+		this.mc = mc;
+		this.vPanel = new VerticalPanel();
+		initWidget(vPanel);
+
+
 		
-		@Override
-		public void onClick(ClickEvent event) {
+		
+
+		okBtn = new Button("Ok");
+		okBtn.setEnabled(false);
+
+		table = new Grid(4,2);
+		table.setWidget(0, 0, new Label("Navn"));
+		table.setWidget(0, 1, nameBox = new TextBox());
+		table.setWidget(1, 0, new Label("Initialer"));
+		table.setWidget(1, 1, iniBox = new TextBox());
+		table.setWidget(2, 0, new Label("CPR"));
+		table.setWidget(2, 1, cprBox = new TextBox());
+		table.setWidget(3, 0, new Label("Rolle"));
+		table.setWidget(3, 1, roleList = new ListBox());
+		
+		roleList.addItem("Operatør");
+		roleList.addItem("Værkfører");
+		roleList.addItem("Farmaceut");
+		roleList.addItem("Administrator");
+	    vPanel.add(table);
+	    
+	    vPanel.add(okBtn);
+	    
+
 			
 		}
-
-	}
-
-private class verifyFields implements KeyUpHandler{
-
-	
-	@Override
-	public void onKeyUp(KeyUpEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
-	
 }
-	
-}
+
+
+
+
+
+
+
+
