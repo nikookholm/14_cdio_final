7811b1a8178c3812e2f4a2ea8e1ca70a4dd64ec2
nehfi <nehfi@localhost.localdomain>
2015-06-09 13:10:17 +0200

    fjernet dato fra pbDTO

--- src/code/database/ProductBatchDTO.java
+++ src/code/database/ProductBatchDTO.java
@@ -7,19 +7,17 @@ public class ProductBatchDTO implements Serializable
 	int pbId;                     // i omraadet 1-99999999
 	int status;					// 0: ikke paabegyndt, 1: under produktion, 2: afsluttet
 	int receptId;
-	int datoId;
 	
 	public ProductBatchDTO()
 	{
 		
 	}
 	
-	public ProductBatchDTO(int pbId, int status, int receptId, int datoId)
+	public ProductBatchDTO(int pbId, int status, int receptId)
 	{
 		this.pbId = pbId;
 		this.status = status;
 		this.receptId = receptId;
-		this.datoId = datoId;
 	}
 	
 	public int getPbId() { return pbId; }
