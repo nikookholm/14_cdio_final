95df19daa527aef3c9d14ae116cdcb9641fb33fc
eggman1979 <kim.d.rasmussen@live.dk>
2015-06-09 23:37:16 +0200

    Der er hul igennem, kan vise createUserView og Kan vise ListUsersView, med brugere

--- src/code/client/controllers/UserController.java
+++ src/code/client/controllers/UserController.java
@@ -14,10 +14,8 @@ import com.google.gwt.user.client.ui.Widget;
 public class UserController {
 
 	private MainController mc;
-
 	ArrayList<UserDTO> users;
-
-	UserDTO userDTO;
+	private UserDTO userDTO;
 
 	public UserController(MainController mc)
 	{
@@ -57,7 +55,20 @@ public class UserController {
 
 	public Widget listUsers()
 	{
-		return new ListUsersView(mc);
+		mc.databaseService.user_table_list(new AsyncCallback<ArrayList<UserDTO>>() {
+
+			@Override
+			public void onFailure(Throwable caught) {
+				// TODO Auto-generated method stub
+
+			}
+			@Override
+			public void onSuccess(ArrayList<UserDTO> result) {
+				sortedUserList(result);
+
+			}
+		});
+		return new ListUsersView(users, mc);
 	}
 
 	public Widget deactivateUser()
@@ -65,4 +76,18 @@ public class UserController {
 		return new Widget();
 	}
 
+	//Metode der omgår at man skal bruge en final i onSucces i ListUsers(), sætter aktive brugere ind i users
+	public void sortedUserList(ArrayList<UserDTO> result) {
+		users = new ArrayList<UserDTO>();
+
+		for (int i = 0; i < result.size(); i++) {
+			if(result.get(i).getActive()){
+				users.add(result.get(i));
+			}
+
+		}
+
+	}
 }
+
+
--- src/code/client/views/ListUsersView.java
+++ src/code/client/views/ListUsersView.java
@@ -2,9 +2,9 @@ package code.client.views;
 
 import java.util.ArrayList;
 
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
-
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -20,15 +20,17 @@ public class ListUsersView extends Composite{
 	VerticalPanel vPanel;
 	Label Header, fillerLabel, presentError;
 	FlexTable table;
+	Anchor edit;
 	
-	public ListUsersView(MainController mc){
+	public ListUsersView(ArrayList<UserDTO> users, MainController mc){
+		this.users = users;
 		this.mc = mc;
 		
 		vPanel = new VerticalPanel();
 		initWidget(vPanel);
 		
 		
-		Header       = new Label("Vis Brugere");
+		Header       = new Label("Vis Brugere ");
 		
 		presentError = new Label("");
 		
@@ -47,6 +49,19 @@ public class ListUsersView extends Composite{
 		table.setText(0, 5, "Rolle");
 		
 	
+		
+		
+		for (int i = 0; i < this.users.size(); i++) {
+			table.setText(i+1, 0, "" + this.users.get(i).getOprId());
+			table.setText(i+1, 1, "" + this.users.get(i).getOprName());
+			table.setText(i+1, 2, "" + this.users.get(i).getIni());
+			table.setText(i+1, 3, "" + this.users.get(i).getCpr());
+			table.setText(i+1, 4, "" + this.users.get(i).getPassword());
+			table.setText(i+1, 5, "" + this.users.get(i).getRole());
+			table.setText(i+1, 6, "" + this.users.get(i).getActive());
+		    table.setWidget(i+1, 8, edit = new Anchor("rediger"));
+			
+		}
 		vPanel.add(table);
 	}
 }
