e7c65712c9e125a25c21517d4e5e6858380fd460
nehfi <nehfi@localhost.localdomain>
2015-06-09 12:48:55 +0200

    tilføjet parametret dato til pbDTO's ikke-tomme-konstruktor

--- src/code/database/ProductBatchDTO.java
+++ src/code/database/ProductBatchDTO.java
@@ -5,17 +5,19 @@ public class ProductBatchDTO
 	int pbId;                     // i omraadet 1-99999999
 	int status;					// 0: ikke paabegyndt, 1: under produktion, 2: afsluttet
 	int receptId;
+	int datoId;
 	
 	public ProductBatchDTO()
 	{
 		
 	}
 	
-	public ProductBatchDTO(int pbId, int status, int receptId)
+	public ProductBatchDTO(int pbId, int status, int receptId, int datoId)
 	{
 		this.pbId = pbId;
 		this.status = status;
 		this.receptId = receptId;
+		this.datoId = datoId;
 	}
 	
 	public int getPbId() { return pbId; }
