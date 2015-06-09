404739f6330771c7bef0e6b24023579a71d04774
amal q <zizja4ka@yahoo.dk>
2015-06-09 15:50:22 +0200

    lavet 2metoder i ingredientController og  listView til ingredienterne

--- src/code/client/controllers/IngredientController.java
+++ src/code/client/controllers/IngredientController.java
@@ -1,13 +1,37 @@
 package code.client.controllers;
 
+import java.util.List;
+
+import code.database.Connector;
+import code.database.IngredientDAO;
+import code.database.IngredientDTO;
+
 public class IngredientController {
 	
+	IngredientDAO ingDAO;
+	IngredientDAO ingDTO;
 	
-	public void createIngredient(){
+	
+	public void createIngredient(IngredientDTO ingDTO) throws Exception {
 		
+		Connector con = new Connector();
+		ingDAO.createIngredient(ingDTO);
+		
+		printIngredient();
 	}
 	
-	public void ingredientList(){
+	private void printIngredient() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<IngredientDTO> ingredientList() throws Exception{
+		
+		Connector con = new Connector();
+		List<IngredientDTO> ingLs = ingDAO.getIngredientList();
+		
+		return ingLs;
+
 		
 	}
 	
--- src/code/client/views/ListIngredientsView.java
+++ src/code/client/views/ListIngredientsView.java
@@ -1,5 +1,67 @@
 package code.client.views;
 
-public class ListIngredientsView {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+import code.client.controllers.IngredientController;
+import code.client.controllers.MainController;
+import code.database.DALException;
+import code.database.IngredientDTO;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class ListIngredientsView extends Composite 
+{
+
+	IngredientController ingCon;
+	MainController mc;
+	VerticalPanel vPanel;
+	Label NameLabel  	 = new Label("ingredient Name");
+	Label IdLabel 		 = new Label("ingredint Id");
+	Label leverandoer	 = new Label("leverandoer");
+
+	TextBox NameBox, IdBox, leverandoerBox;
+
+	Grid tabel;
+	FlexTable flexTabel;
+
+	ArrayList<IngredientDTO> ingls;
+
+	public ListIngredientsView(MainController mc, ArrayList<IngredientDTO> ingls) throws DALException
+	{
+		this.mc = mc;
+		this.vPanel = new VerticalPanel();
+		initWidget(this.vPanel);
+		this.ingls = ingls;
+
+		this.tabel = new Grid(1, 3);
+		
+		flexTabel = new FlexTable();
+
+
+		tabel.setWidget(0, 0, NameLabel);
+		tabel.setWidget(0, 1, IdLabel);
+		tabel.setWidget(0, 2, leverandoer);
+		
+		vPanel.add(tabel);
+
+
+
+		for (int i = 0; i < ingls.size(); i++ ) {
+
+			flexTabel.setWidget(i+1, 0, new Label(ingls.get(i).getIngredientId()   + ""));
+			flexTabel.setWidget(i+1, 1, new Label(ingls.get(i).getIngredientName() + ""));
+			flexTabel.setWidget(i+1, 2, new Label(ingls.get(i).getLeverandoer()    + ""));
+		}
+		
+		
+		vPanel.add(flexTabel);
+	}
 
 }
