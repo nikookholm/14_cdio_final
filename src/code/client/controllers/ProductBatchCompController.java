c9e4ded743c3cdfbcb18508487fe27206f3be60a
JacobLA <jacob_jau@hotmail.com>
2015-06-10 14:09:53 +0200

    PBComp-cont. færdig for, hvad den skal gøre. ListPBCompView skulle nu gerne vise en liste over PBComps og en tilbage til main-view knap.

--- src/code/client/controllers/ProductBatchCompController.java
+++ src/code/client/controllers/ProductBatchCompController.java
@@ -6,8 +6,7 @@ import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Widget;
 
-import code.client.views.CreateProductBatchView;
-import code.client.views.ListProductBatchView;
+import code.client.views.ListProductBatchCompView;
 import code.database.*;
 
 
@@ -23,8 +22,29 @@ public class ProductBatchCompController
 	
 	public Widget listProductBatchComp()
 	{
-		mc.databaseService.productBatchComp_table_list(new AsyncCallBack <ArrayList<ProductBatchCompDTO>>(){
+		mc.databaseService.productBatchComp_table_list(new AsyncCallback<ArrayList<ProductBatchCompDTO>>(){
 			
+			@Override
+			public void onFailure(Throwable caught)
+			{
+				Window.alert("Kunne ikke hente liste fra server" + caught.getMessage());
+			}
+			@Override
+			public void onSuccess(ArrayList<ProductBatchCompDTO> list)
+			{
+				//Success
+			}
+		});
+		return new ListProductBatchCompView(pbCompDTO, mc);
+	}
+	
+	public void SortedPBCompList(ArrayList<ProductBatchCompDTO> list)
+	{
+		list = new ArrayList<ProductBatchCompDTO>();
+		
+		for(int i = 0; i < list.size(); i++)
+		{
+			list.add(list.get(i));
 		}
 	}
 }
--- src/code/client/views/ListProductBatchCompView.java
+++ src/code/client/views/ListProductBatchCompView.java
@@ -7,32 +7,41 @@ import code.client.DatabaseServiceAsync;
 import code.client.DatabaseService;
 import code.database.*;
 import code.client.controllers.MainController;
+import code.client.controllers.ProductBatchCompController;
 import code.client.controllers.ProductBatchController;
 import code.database.DALException;
 import code.server.DatabaseServiceImpl;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
 public class ListProductBatchCompView extends Composite {
 
-	ProductBatchController pbC;
+	ProductBatchCompController pbCompC;
 	MainController mc;
 	VerticalPanel VPanel;
-	
-	ArrayList<ProductBatchCompDTO> ls;
+	Button backButton;
+	FlexTable ft;
 
-	public ListProductBatchCompView(final MainController mc) throws DALException
+	ArrayList<ProductBatchCompDTO> list;
+
+	public ListProductBatchCompView(ProductBatchCompDTO pbCompDTO, MainController mc)
 	{
 		this.mc = mc;
 		VPanel = new VerticalPanel();
 		VPanel.setHeight("100px");
 		initWidget(this.VPanel);
 
-		final FlexTable ft = new FlexTable();
+		backButton = new Button("Tilbage");
+		backButton.setEnabled(true);
+
+		this.ft = new FlexTable();
 		ft.getCellFormatter().setWidth(0, 0, "100px");
 		ft.getCellFormatter().setWidth(0, 1, "100px");
 		ft.getCellFormatter().setWidth(0, 2, "100px");
@@ -40,35 +49,29 @@ public class ListProductBatchCompView extends Composite {
 		ft.getCellFormatter().setWidth(0, 4, "100px");
 
 		ft.setTitle("ProductBatchCompListView");
-
 		ft.setText(0, 0, "pbID");
 		ft.setText(0, 1, "rbID");
 		ft.setText(0, 2, "tara");
 		ft.setText(0, 3, "netto");
 		ft.setText(0, 4, "oprID");
 
-
-		mc.databaseService.productBatchComp_table_list(new AsyncCallback<ArrayList<ProductBatchCompDTO>>(){
-
-			@Override
-			public void onFailure(Throwable caught) {
-				Window.alert("Server fejl!" + caught.getMessage());
-			}
-
-			@Override
-			public void onSuccess(ArrayList<ProductBatchCompDTO> result) {
-				for (int i=0; i < result.size(); i++) {
-					ft.setText(i+1, 0, "" + result.get(i).getPbId());
-					ft.setText(i+1, 1, "" + result.get(i).getRbId());
-					ft.setText(i+1, 2, "" + result.get(i).getTara());
-					ft.setText(i+1, 3, "" + result.get(i).getNetto());
-					ft.setText(i+1, 4, "" + result.get(i).getOprId());
-				}
-
-			}
-
-		});
-		
+		for (int i=0; i < list.size(); i++) {
+			ft.setText(i+1, 0, "" + list.get(i).getPbId());
+			ft.setText(i+1, 1, "" + list.get(i).getRbId());
+			ft.setText(i+1, 2, "" + list.get(i).getTara());
+			ft.setText(i+1, 3, "" + list.get(i).getNetto());
+			ft.setText(i+1, 4, "" + list.get(i).getOprId());
+		}
 		VPanel.add(ft);
-	}	
+		backButton.addClickHandler(new backClickHandler());
+	}
+
+	private class backClickHandler implements ClickHandler
+	{
+		@Override
+		public void onClick(ClickEvent event) 
+		{	
+			mc.show(new MainView(mc));
+		}
+	}
 }
