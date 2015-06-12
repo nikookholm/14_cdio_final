74f417acf0523b378b9556c3a283368a313ca74b
JacobLA <jacob_jau@hotmail.com>
2015-06-12 15:39:44 +0200

    Tilføjet kode til IBController. Tilhørende klasser skal laves. CreateIBView, ListIBView

--- src/code/client/controllers/IngredientBatchController.java
+++ src/code/client/controllers/IngredientBatchController.java
@@ -1,5 +1,78 @@
 package code.client.controllers;
 
-public class IngredientBatchController {
+import java.util.ArrayList;
 
+import code.client.views.CreateIngredientBatchView;
+import code.client.views.CreateIngredientView;
+import code.client.views.ListIngredientBatchView;
+import code.client.views.ListProductBatchCompView;
+import code.database.IngredientBatchDTO;
+import code.database.IngredientDTO;
+
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.Widget;
+
+public class IngredientBatchController 
+{
+	MainController mc;
+	ArrayList<IngredientBatchDTO> ingrBatchDTO;
+
+	public IngredientBatchController(MainController mc)
+	{
+		this.mc = mc;
+	}
+	
+	public Widget createIngredientBatch(IngredientBatchDTO ingrBatchDTO) {
+
+		if(ingrBatchDTO!= null){
+			
+			// opret dto i database
+			mc.databaseService.ingredientBatch_table_create(ingrBatchDTO, new AsyncCallback<Void>() {
+				
+				@Override
+				public void onSuccess(Void result) {
+						
+				}
+				
+				@Override
+				public void onFailure(Throwable caught) {
+					
+				}
+			});			
+			return new CreateIngredientBatchView(mc, ingrBatchDTO);
+		}else{
+			
+			return new CreateIngredientBatchView(mc, null);
+		}
+	}
+	
+	public Widget listIngredientBatch()
+	{
+		mc.databaseService.ingredientBatch_table_list(new AsyncCallback<ArrayList<IngredientBatchDTO>>(){
+			
+			@Override
+			public void onFailure(Throwable caught)
+			{
+				Window.alert("Kunne ikke hente liste fra server" + caught.getMessage());
+			}
+			@Override
+			public void onSuccess(ArrayList<IngredientBatchDTO> list)
+			{
+				SortedIBList(list);
+			}
+		});
+		//return new ListIngredientBatchView(ingrBatchDTO, mc);
+		return null;
+	}
+	
+	public void SortedIBList(ArrayList<IngredientBatchDTO> list)
+	{
+		ingrBatchDTO = new ArrayList<IngredientBatchDTO>();
+		
+		for(int i = 0; i < ingrBatchDTO.size(); i++)
+		{
+			ingrBatchDTO.add(ingrBatchDTO.get(i));
+		}
+	}
 }
