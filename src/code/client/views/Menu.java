99d44c38e78fb1792c250dbb9e6123f366fdff29
Niko <nikooftheworld@gmail.com>
2015-06-16 13:02:06 +0200

    Tilføjet metode til logout

--- src/code/client/views/Menu.java
+++ src/code/client/views/Menu.java
@@ -7,6 +7,7 @@ import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -29,9 +30,9 @@ public class Menu extends Composite {
 		
 		if (mc.getUser() != null)
 		{
-			logoutPanel.add(new Label("Du er logget ind som "));
+			logoutPanel.add(new HTML("Du er logget ind som&nbsp;"));
 			logoutPanel.add(new Label(mc.getUser().getOprName()));
-			logoutPanel.add(new Label(" ("));
+			logoutPanel.add(new HTML("&nbsp;("));
 			logoutPanel.add(logoutAnchor);
 			logoutPanel.add(new Label(")"));
 			
@@ -43,6 +44,8 @@ public class Menu extends Composite {
 			CellFormatter cf =  table.getCellFormatter();
 			cf.setWidth(0, 1, "100%;");
 			cf.setHorizontalAlignment(0, 1,  HasHorizontalAlignment.ALIGN_RIGHT);
+			
+			logoutAnchor.addClickHandler(new logoutHandler());
 		}
 		
 		initWidget(table);
