5b40a89ddec25bb80f6fff9c51e57a40cf9989ad
JacobLA <jacob_jau@hotmail.com>
2015-06-17 15:18:25 +0200

    Der checkes nu for i create, om råvare id eller råvarebatch id findes i forvejen.

--- src/code/client/controllers/IngredientBatchController.java
+++ src/code/client/controllers/IngredientBatchController.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import code.client.views.CreateIngredientBatchView;
 import code.client.views.ListIngredientBatchView;
 import code.database.IngredientBatchDTO;
+import code.database.ReceptDTO;
 
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
@@ -14,46 +15,72 @@ public class IngredientBatchController
 {
 	MainController mc;
 	ArrayList<IngredientBatchDTO> ingrBatchDTO;
+	IngredientBatchDTO ibDTO;
+	boolean booln = false;
 
 	public IngredientBatchController(MainController mc)
 	{
 		this.mc = mc;
 	}
-	
-	public Widget createIngredientBatch(IngredientBatchDTO ingrBatchDTO) {
 
+	public Widget createIngredientBatch(IngredientBatchDTO ingrBatchDTO) 
+	{
+		this.ibDTO = ingrBatchDTO;
 		if(ingrBatchDTO!= null){
-			
+			int checkIngId 		= ingrBatchDTO.getIngredientId();
+			final int checkIbId	= ingrBatchDTO.getRbId();
 			// opretter dto i database
-			mc.databaseService.ingredientBatch_table_create(ingrBatchDTO, new AsyncCallback<Void>() {
-				
+			mc.databaseService.ingredientBatch_table_get(checkIngId, new AsyncCallback<IngredientBatchDTO>(){
 				@Override
-				public void onSuccess(Void result) {
-						
+				public void onSuccess(IngredientBatchDTO result) {
+					mc.databaseService.ingredientBatch_table_get(checkIbId, new AsyncCallback<IngredientBatchDTO>(){
+						@Override
+						public void onSuccess(IngredientBatchDTO result) {
+							booln = true;
+						}
+						@Override
+						public void onFailure(Throwable caught) {
+							booln = false;
+						}
+					});
 				}
-				
+
 				@Override
 				public void onFailure(Throwable caught) {
-					
-				}
-			});			
+					booln = false;
+				}		
+			});
+
+			if(booln == true){
+				mc.databaseService.ingredientBatch_table_create(ingrBatchDTO, new AsyncCallback<Void>() {
+
+					@Override
+					public void onSuccess(Void result) {
+
+					}
+
+					@Override
+					public void onFailure(Throwable caught) {
+
+					}
+				});	
+			}
 			return new CreateIngredientBatchView (ingrBatchDTO, mc);
 		}else{
-			
 			return new CreateIngredientBatchView(null, mc);
 		}
 	}
-	
+
 	public Widget listIngredientBatch()
 	{
 		mc.databaseService.ingredientBatch_table_list(new AsyncCallback<ArrayList<IngredientBatchDTO>>(){
-	
+
 			@Override
 			public void onSuccess(ArrayList<IngredientBatchDTO> list)
 			{
 				ingrBatchDTO = list;
 			}
-			
+
 			@Override
 			public void onFailure(Throwable caught)
 			{
