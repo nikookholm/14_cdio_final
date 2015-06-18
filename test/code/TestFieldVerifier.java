23de8ad151b48aec654e7565c08cb1f6e59c6bfe
amal q <zizja4ka@yahoo.dk>
2015-06-18 14:45:32 +0200

    startet at teste FieldVerifier

---
+++ test/code/TestFieldVerifier.java
@@ -0,0 +1,41 @@
+package code;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import code.shared.FieldVerifier;
+
+public class TestFieldVerifier {
+	
+	
+	
+	@Test
+	public void testIsValidName(){
+		boolean actual = FieldVerifier.isValidName(" ");
+		
+		assertFalse(actual);
+		
+	}
+	
+	@Test
+	public void testIsValidNameLength(){
+		
+		boolean actual = FieldVerifier.isValidName("qwertyuiopasdfghjklzx");
+		
+		assertFalse(actual);
+		
+	}
+	
+	@Test
+	public void testIsValidName2Short(){
+		boolean actual = FieldVerifier.isValidName("x");
+		
+		assertFalse(actual);
+		
+	}
+	
+	
+	
+
+}
