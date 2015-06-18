36c37fe795ad54f114e6bbb01bf7ad5fd9ea3829
amal q <zizja4ka@yahoo.dk>
2015-06-18 20:19:56 +0200

    3 metodeterne fra FieldVerifier blev testet i forskælig måde

--- test/code/TestFieldVerifier.java
+++ test/code/TestFieldVerifier.java
@@ -8,6 +8,7 @@ import code.shared.FieldVerifier;
 
 public class TestFieldVerifier {
 	
+	
 	@Test
 	public void testIsValidName()
 	{
@@ -34,5 +35,94 @@ public class TestFieldVerifier {
 		assertFalse(actual);
 		
 	}
+	
+	@Test
+	public void testIsCPRwithSpace(){
+		
+		boolean actual = FieldVerifier.isCPRValid(" ");
+		
+		assertFalse(actual);
+	}
+	
+	@Test
+	public void testIsCPRwithLetter(){
+		
+		boolean actual = FieldVerifier.isCPRValid("x");
+		
+		assertFalse(actual);
+		
+	}
+	
+	@Test
+	public void testIsCPRValid10(){
+		
+		boolean actual = FieldVerifier.isCPRValid("1234567890");
+		
+		assertTrue(actual);
+		
+	}
+	
+	@Test
+	public void testIsCPRValid2(){
+		
+		boolean actual = FieldVerifier.isCPRValid("22");
+		
+		assertFalse(actual);
+	}
+	
+	@Test
+	public void testInitials(){
+		
+		boolean actual = FieldVerifier.isInitialsValid("333");
+		
+		assertTrue(actual);
+		
+	}
+	
+	@Test
+	public void testInitialsWith1Char(){
+		
+		boolean actual = FieldVerifier.isInitialsValid("3");
+		
+		assertFalse(actual);
+		
+	}
+	
+	@Test
+	public void testInitialsWith19Char(){
+		
+		boolean actual = FieldVerifier.isInitialsValid("3asfasf45737y334%&&");
+		
+		assertFalse(actual);
+		
+	}
+	
+	
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
