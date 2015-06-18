9163846b62f367d699d7b835f56534ef93afbff6
nehfi <nehfi@localhost.localdomain>
2015-06-18 11:55:52 +0200

    updateret listusersview

--- src/code/client/views/ListUsersView.java
+++ src/code/client/views/ListUsersView.java
@@ -27,16 +27,9 @@ public class ListUsersView extends Composite{
 	ArrayList<UserDTO> users;
 
 	VerticalPanel vPanel;
-	Label idLabel		= new Label("ID");
-	Label nameLabel		= new Label("Navn");
-	Label iniLabel		= new Label("Initialer");
-	Label cprLabel		= new Label("CPR");
-	Label passwordLabel	= new Label("Password");
-	Label roleLabel		= new Label("Rolle");
-	Label Header		= new Label("Brugere");
-	Label fillerLabel, presentError;
-	FlexTable table;
-	Anchor edit,ok, prevCancel = null;
+	Label headerLabel, idLabel, nameLabel, iniLabel, cprLabel, passwordLabel, roleLabel;
+	FlexTable flex;
+	Anchor edit, ok, prevCancel = null;
 	int selectedRow;
 	TextBox nameBox, iniBox, pwBox;
 	ListBox roleBox;
@@ -47,57 +40,63 @@ public class ListUsersView extends Composite{
 	boolean iniCheck  = false;
 	boolean cprCheck  = false;
 
-	public ListUsersView(ArrayList<UserDTO> users, MainController mc){
+	public ListUsersView(ArrayList<UserDTO> users, MainController mc)
+	{
 		this.users = users;
 		this.mc = mc;
 		vPanel = new VerticalPanel();
-		backButton = new Button("Tilbage");
-		backButton.setEnabled(true);
-
-		presentError = new Label("");
-
-		table = new FlexTable();
-		vPanel.add(Header);
-		Header.setStyleName("caption");
-		vPanel.add(presentError);
-		table.setWidget(0, 0, idLabel);
+		
+		headerLabel		= new Label("Brugere");		
+		idLabel			= new Label("ID");
+		nameLabel		= new Label("Navn");
+		iniLabel		= new Label("Initialer");
+		cprLabel		= new Label("CPR");
+		passwordLabel	= new Label("Password");
+		roleLabel		= new Label("Rolle");
+		
+		flex = new FlexTable();
+		flex.setWidget(0, 0, idLabel);
+		flex.setWidget(0, 1, nameLabel);
+		flex.setWidget(0, 2, iniLabel);
+		flex.setWidget(0, 3, cprLabel);
+		flex.setWidget(0, 4, passwordLabel);
+		flex.setWidget(0, 5, roleLabel);
+		headerLabel.setStyleName("caption");
 		idLabel.setStyleName("input-text");
-		table.setWidget(0, 1, nameLabel);
 		nameLabel.setStyleName("input-text");
-		table.setWidget(0, 2, iniLabel);
 		iniLabel.setStyleName("input-text");
-		table.setWidget(0, 3, cprLabel);
 		cprLabel.setStyleName("input-text");
-		table.setWidget(0, 4, passwordLabel);
-		passwordLabel.setStyleName("input-text");
-		table.setWidget(0, 5, roleLabel);
+		passwordLabel.setStyleName("input-text");		
 		roleLabel.setStyleName("input-text");
-		table.getCellFormatter().setWidth(0, 0, "150px;");
-		table.getCellFormatter().setWidth(0, 1, "150px;");
-		table.getCellFormatter().setWidth(0, 2, "150px;");
-		table.getCellFormatter().setWidth(0, 3, "150px;");
-		table.getCellFormatter().setWidth(0, 4, "150px;");
-		table.getCellFormatter().setWidth(0, 5, "150px;");
+		flex.getCellFormatter().setWidth(0, 0, "150px;");
+		flex.getCellFormatter().setWidth(0, 1, "150px;");
+		flex.getCellFormatter().setWidth(0, 2, "150px;");
+		flex.getCellFormatter().setWidth(0, 3, "150px;");
+		flex.getCellFormatter().setWidth(0, 4, "150px;");
+		flex.getCellFormatter().setWidth(0, 5, "150px;");
 		
 		for (int i = 0; i < this.users.size(); i++) {
-			table.setText(i+1, 0,	"" + this.users.get(i).getOprId());
-			table.setText(i+1, 1, 	"" + this.users.get(i).getOprName());
-			table.setText(i+1, 2, 	"" + this.users.get(i).getIni());
-			table.setText(i+1, 3,  	"" + this.users.get(i).getCpr());
-			table.setText(i+1, 4, 	"" + this.users.get(i).getPassword());
-			table.setText(i+1, 5, 	"" + parseRole(this.users.get(i).getRole()));
-			table.setText(i+1, 6, 	"" + this.users.get(i).getActive());
+			flex.setText(i+1, 0,	"" + this.users.get(i).getOprId());
+			flex.setText(i+1, 1, 	"" + this.users.get(i).getOprName());
+			flex.setText(i+1, 2, 	"" + this.users.get(i).getIni());
+			flex.setText(i+1, 3,  	"" + this.users.get(i).getCpr());
+			flex.setText(i+1, 4, 	"" + this.users.get(i).getPassword());
+			flex.setText(i+1, 5, 	"" + parseRole(this.users.get(i).getRole()));
+			flex.setText(i+1, 6, 	"" + this.users.get(i).getActive());
 
-			table.setWidget(i+1, 7, edit = new Anchor("rediger"));
+			flex.setWidget(i+1, 7, edit = new Anchor("Redigér"));
 			edit.addClickHandler(new EditButtonHandler());
 		}
-
-		vPanel.add(table);
+		
+		backButton = new Button("Tilbage");	
+		backButton.setEnabled(true);
+		backButton.addClickHandler(new backClickHandler());
+		
+		vPanel.add(headerLabel);
+		vPanel.add(flex);
 		vPanel.add(backButton);
 		vPanel.setCellHorizontalAlignment(backButton, HasHorizontalAlignment.ALIGN_CENTER);
 		initWidget(this.vPanel);
-		
-		backButton.addClickHandler(new backClickHandler());
 		
 		nameBox 	= new TextBox();
 		iniBox 		= new TextBox();
@@ -118,13 +117,13 @@ public class ListUsersView extends Composite{
 				prevCancel.fireEvent(new ClickEvent(){});
 			}
 			//Finds the row the user clicked
-			selectedRow = table.getCellForEvent(event).getRowIndex();
+			selectedRow = flex.getCellForEvent(event).getRowIndex();
 
-			nameBox.setText(table.getText(selectedRow, 1));
+			nameBox.setText(flex.getText(selectedRow, 1));
 			//nameBox.setWidth("150px");
-			iniBox.setText(table.getText(selectedRow, 2));
+			iniBox.setText(flex.getText(selectedRow, 2));
 			//iniBox.setWidth("40px");
-			pwBox.setText(table.getText(selectedRow, 4));
+			pwBox.setText(flex.getText(selectedRow, 4));
 			//pwBox.setWidth("100px");
 
 			roleBox.clear();
@@ -132,42 +131,40 @@ public class ListUsersView extends Composite{
 			roleBox.addItem("Værkfører");
 			roleBox.addItem("Farmaceut");
 			roleBox.addItem("Administrator");
-			roleBox.setItemSelected(reverseParseRole(table.getText(selectedRow, 5)), true);
-			deactivate.setValue(table.getText(selectedRow, 6).equals("true"));
+			roleBox.setItemSelected(reverseParseRole(flex.getText(selectedRow, 5)), true);
+			deactivate.setValue(flex.getText(selectedRow, 6).equals("true"));
 
-			table.setWidget(selectedRow, 1, nameBox);
+			flex.setWidget(selectedRow, 1, nameBox);
 			nameBox.addKeyUpHandler(new NameBoxHandler());
-			table.setWidget(selectedRow, 2, iniBox);
+			flex.setWidget(selectedRow, 2, iniBox);
 			iniBox.addKeyUpHandler(new IniBoxHandler());
-			table.setWidget(selectedRow, 4, pwBox);
-			table.setWidget(selectedRow, 5, roleBox);
-			table.setWidget(selectedRow, 6, deactivate);
+			flex.setWidget(selectedRow, 4, pwBox);
+			flex.setWidget(selectedRow, 5, roleBox);
+			flex.setWidget(selectedRow, 6, deactivate);
 
 			nameBox.setFocus(true);
 
 			final Anchor edit =  (Anchor) event.getSource();
-			final int id = Integer.parseInt(table.getText(selectedRow, 0));
+			final int id = Integer.parseInt(flex.getText(selectedRow, 0));
 			final String name = nameBox.getText();
 			final String ini = iniBox.getText();
-			final String cpr = (table.getText(selectedRow, 3));
+			final String cpr = (flex.getText(selectedRow, 3));
 			final String pw = pwBox.getText();
 			final String role = roleBox.getItemText(roleBox.getTabIndex());
 			final int roleToSave = roleBox.getTabIndex();
 			final String isActive = "" + parseCheckBox(deactivate.getFormValue());
 			activityParser = isActive;
 
-			final Anchor ok = new Anchor("ok");
+			final Anchor ok = new Anchor("OK");
 			ok.addClickHandler(new ClickHandler() {
-
 				@Override
 				public void onClick(ClickEvent event) {
-					//gemmer de opdateret værdier
 					UserDTO updatedUser = new UserDTO(id, nameBox.getText(), iniBox.getName(), cpr, pwBox.getText(), roleBox.getSelectedIndex(), deactivate.getValue());
 					mc.getUserController().updateUser(updatedUser);
 				}
 			});
 
-			Anchor cancel = new Anchor("cancel");
+			Anchor cancel = new Anchor("Annullér");
 			prevCancel = cancel;
 
 			cancel.addClickHandler(new ClickHandler() {
@@ -181,26 +178,24 @@ public class ListUsersView extends Composite{
 					iniBox.setText(ini);
 					iniBox.fireEvent(new KeyUpEvent() {});
 
-					table.setText(selectedRow, 1,	name);
-					table.setText(selectedRow, 2, 	ini);
-					table.setText(selectedRow, 4, 	pw);
-					table.setText(selectedRow, 5, 	role);
-					table.setText(selectedRow, 6, 	isActive);
-					table.setWidget(selectedRow, 7, edit);
+					flex.setText(selectedRow, 1,	name);
+					flex.setText(selectedRow, 2, 	ini);
+					flex.setText(selectedRow, 4, 	pw);
+					flex.setText(selectedRow, 5, 	role);
+					flex.setText(selectedRow, 6, 	isActive);
+					flex.setWidget(selectedRow, 7, 	edit);
 
-					table.clearCell(selectedRow, 8);
-
+					flex.clearCell(selectedRow, 8);
 					prevCancel = null;
 				}
 			}); 
-			table.setWidget(selectedRow, 7, cancel);
-			table.setWidget(selectedRow, 8, ok);
+			flex.setWidget(selectedRow, 7, cancel);
+			flex.setWidget(selectedRow, 8, ok);
 		}
-
 	}
 
-	private String parseRole(int roleNo){
-
+	private String parseRole(int roleNo)
+	{
 		if(roleNo == 0) return "Operatør";
 		else if(roleNo == 1) return "Værkfører";
 		else if(roleNo == 2) return "Farmaceut";
@@ -208,7 +203,8 @@ public class ListUsersView extends Composite{
 		else return null;
 	}
 
-	private int reverseParseRole(String roleName){
+	private int reverseParseRole(String roleName)
+	{
 		if("Operatør".equals(roleName)) return 0;
 		else if("Værkfører".equals(roleName)) return 1;
 		else if("Farmaceut".equals(roleName)) return 2;
@@ -216,44 +212,41 @@ public class ListUsersView extends Composite{
 		else return -1;
 	}
 
-	private boolean parseCheckBox(String on){
+	private boolean parseCheckBox(String on)
+	{
 		if(on.equalsIgnoreCase("on")) return true;
 		else return false;
 	}
 
 	private class NameBoxHandler implements KeyUpHandler{
-
 		@Override
 		public void onKeyUp(KeyUpEvent event) {
-			if(!FieldVerifier.isValidName(nameBox.getText())){
-				nameBox.setStyleName("gwt-TextBox-invalidEntry");
-				nameCheck = false;
-			}
-			else{
-				nameBox.removeStyleName("gwt-TextBox-invalidEntry");
+//			if(!FieldVerifier.isValidName(nameBox.getText())){
+//				nameBox.setStyleName("gwt-TextBox-invalidEntry");
+//				nameCheck = false;
+//			}
+//			else{
+//				nameBox.removeStyleName("gwt-TextBox-invalidEntry");
 				nameCheck = true;
-			}
+//			}
 		}
 	}
 	
 	private class IniBoxHandler implements KeyUpHandler{
-
 		@Override
 		public void onKeyUp(KeyUpEvent event) {
-			if(!FieldVerifier.isInitialsValid(iniBox.getText())){
-				iniBox.setStyleName("gwt-TextBox-invalidEntry");
-				iniCheck = false;
-			}
-			else{
-				iniBox.removeStyleName("gwt-TextBox-invalidEntry");
+//			if(!FieldVerifier.isInitialsValid(iniBox.getText())){
+//				iniBox.setStyleName("gwt-TextBox-invalidEntry");
+//				iniCheck = false;
+//			}
+//			else{
+//				iniBox.removeStyleName("gwt-TextBox-invalidEntry");
 				iniCheck = true;
-			}
-
+//			}
 		}
 	}
 
-	private class backClickHandler implements ClickHandler
-	{
+	private class backClickHandler implements ClickHandler{
 		@Override
 		public void onClick(ClickEvent event) {
 			mc.show(new MainView(mc));
